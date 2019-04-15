4610cd7c05589e9d155d4f881f45380715445e64
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-04-15 15:13:02 +0200

    Add files via upload

---
+++ Exercise.4/Exercise.4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Exercise._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Speed Limmit:");
+            var speedlimit = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Speed of Car:");
+            var Car = Convert.ToInt32(Console.ReadLine());
+
+            if (Car < speedlimit)
+                Console.WriteLine("OK");
+            else
+            {
+                const int KmperDemeritpoint = 5;
+                var Demeritpoints = (Car - speedlimit) / KmperDemeritpoint;
+                if (Demeritpoints < 12) 
+                Console.WriteLine("License Suspended");
+                else 
+                Console.WriteLine("License suspended "+Demeritpoints);
+
+            }
+
+        }
+    }
+}
+
